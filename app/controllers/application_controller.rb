d029bf8a25106af50ca0f4a27687026ab18d09d9
Alexandre Lopes <alexandre9999lopes@gmail.com>
2021-11-28 20:04:07 -0300

    improv: adds exceptions rescues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    render json: { message: e }, status: :bad_request
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { message: e }, status: :not_found
+  end
+
+  rescue_from SpaceProbe::Exceptions::InvalidCommands do |e|
+    render json: { message: e }, status: :bad_request
+  end
 end
