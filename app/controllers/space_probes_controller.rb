d75e93ee90f575c2c46ea0acfe8a7f7a2ef834ec
Alexandre Lopes <alexandre9999lopes@gmail.com>
2021-11-28 20:04:26 -0300

    feat: adds space probes controller

---
+++ app/controllers/space_probes_controller.rb
@@ -0,0 +1,35 @@
+class SpaceProbesController < ApplicationController
+  before_action :set_space_probe, only: [:show, :reset, :move]
+
+  def index
+    render json: SpaceProbe.all
+  end
+  
+  def create
+    @space_probe = SpaceProbe.create(position_x: 0, position_y: 0, front_direction: 'D')
+    
+    render json: @space_probe, status: :created
+  end
+  
+  def show
+    render json: @space_probe
+  end
+
+  def move
+    @space_probe = SpaceProbe::Move.new(space_probe: @space_probe, commands: params[:commands]).call
+
+    render json: @space_probe
+  end
+  
+  def reset
+    @space_probe.update(position_x: 0, position_y: 0, front_direction: 'D')
+
+    head :ok
+  end
+
+  private
+
+  def set_space_probe
+    @space_probe = SpaceProbe.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :space_probes, only: [:index, :create] do
+    collection do
+      get '/:id/position', to: 'space_probes#show'
+      put '/:id/initial_position', to: 'space_probes#reset'
+      put '/:id/move', to: 'space_probes#move'
+    end
+  end
 end
