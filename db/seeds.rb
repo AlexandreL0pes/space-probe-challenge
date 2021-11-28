d79ced817f20c5bc148fec79fa677cbac578f5db
Alexandre Lopes <alexandre9999lopes@gmail.com>
2021-11-28 17:59:04 -0300

    test: adds space probes in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+SpaceProbe.create(position_x: 0, position_y: 1, front_direction: 'D')
+SpaceProbe.create(position_x: 4, position_y: 4, front_direction: 'B')
+SpaceProbe.create(position_x: 0, position_y: 0, front_direction: 'D')
